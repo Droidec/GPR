5d7891dfbd310e6eb6afe35c969681fbd974e65d
Droidec <derovor@gmail.com>
2020-12-27 18:01:15 +0100

    Improve grapher + Update rules

--- Makefile
+++ Makefile
@@ -105,8 +105,7 @@ uninstall:
 
 .PHONY: graph
 graph:
-	python3 $(GRAPH_REP)/$(GRAPH_NAME) $(INC_REP) $(SRC_REP) -n $(LIB_NAME) -m module --exist-only -o
-	mv *.gv *.pdf $(GRAPH_REP)/
+	python3 $(GRAPH_REP)/$(GRAPH_NAME) $(INC_REP) $(SRC_REP) -n $(LIB_NAME) -m module -o $(GRAPH_REP) --known-only --view
 
 .PHONY: test
 test: $(TEST_REP) $(BIN_TEST_REP) $(EXECUTABLES)
--- extras/graph/grapher.py
+++ extras/graph/grapher.py
@@ -1,7 +1,7 @@
-"""Graph module
+"""Grapher
 
-This module scans a library and produces PDF files
-representing dependencies
+This module scans a set of C-like files and produces a
+Graphviz and PDF file showing file dependencies
 
 You can alter rendered file(s) with command-line options
 """
@@ -42,32 +42,44 @@ class GraphFile(object):
 
     def __get_include_list(self):
         """Get a list of includes from a file"""
-        return [x[1:-1] for line in open(f'{self.path}{self.name}{self.ext}', 'r') for x in re.findall(r'^#\s*include\s+(\S+)', line) if x]
+        try:
+            return [x[1:-1] for line in open(f'{self.path}{self.name}{self.ext}', 'r') for x in re.findall(r'\s*#\s*include\s+(\S+)', line) if x]
+        except:
+            # File is not readable
+            return []
 
-class GraphLibrary(object):
-    """GraphLibrary class
+class GraphSet(object):
+    """GraphSet class
 
-    Describe a library and its dependencies
+    Describe a set and its dependencies
     """
-    def __init__(self, dir_list, name='grapher', mode='file', existonly=False):
+    def __init__(self, dir_list, name='set', mode='file', output='.', known_only=False):
         self.name = name
         self.mode = mode
-        self.existonly = existonly
+
+        if not os.path.isdir(output):
+            raise ValueError(f'\'{output}\' is not a directory')
+
+        self.output = output + '/' if not output.endswith('/') else output
+        self.known_only = known_only
         self.files = []
 
         # Get files and their dependencies
         for directory in dir_list:
+            if not os.path.isdir(directory):
+                raise ValueError(f'\'{directory}\' is not a directory')
+
             path = directory + '/' if not directory.endswith('/') else directory
             self.files.extend(self.__get_dir_files(path))
 
     def __str__(self):
-        if self.existonly is False:
+        if self.known_only is False:
             return '\n'.join([file.__str__() for file in self.files])
         else:
             return '\n'.join([file.__str__(self.files) for file in self.files])
 
     def __repr__(self):
-        if self.existonly is False:
+        if self.known_only is False:
             return '\n'.join([file.__repr__() for file in self.files])
         else:
             return '\n'.join([file.__repr__(self.files) for file in self.files])
@@ -93,21 +105,21 @@ class GraphLibrary(object):
     def render(self, cleanup=False, view=False):
         """Render graph as a DOT/PDF file"""
 
-        # Write DOT file
-        dot = open(f'{self.name}.gv', 'w')
+        # Render DOT file
+        dot = open(f'{self.output}{self.name}.gv', 'w')
 
-        dot.write(f'digraph {self.name} {{\n')
+        dot.write(f'digraph "{self.name}" {{\n')
 
         if self.mode == 'file':
             for file in self.files:
-                if self.existonly is False:
+                if self.known_only is False:
                     dot.write(file.__str__() + '\n')
                 else:
                     dot.write(file.__str__(self.files) + '\n')
 
         if self.mode == 'module':
             for file in self.files:
-                if self.existonly is False:
+                if self.known_only is False:
                     dot.write(file.__repr__() + '\n')
                 else:
                     dot.write(file.__repr__(self.files) + '\n')
@@ -117,38 +129,39 @@ class GraphLibrary(object):
 
         # Render PDF file (Graphviz must be installed)
         if shutil.which('dot') is None:
-            os.remove(f'{self.name}.gv')
+            os.remove(f'{self.output}{self.name}.gv')
             raise OSError('Consider installing Graphviz (dot executable not found)')
 
-        subprocess.run(['dot', '-Tpdf', f'{self.name}.gv', '-o', f'{self.name}.pdf'], check=True)
+        subprocess.run(['dot', '-Tpdf', f'{self.output}{self.name}.gv', '-o', f'{self.output}{self.name}.pdf'], check=True)
 
         if cleanup is True:
-            os.remove(f'{self.name}.gv')
+            os.remove(f'{self.output}{self.name}.gv')
 
         if view is True:
-            self.__default_open(f'{self.name}.pdf')
+            self.__default_open(f'{self.output}{self.name}.pdf')
 
 if __name__ == "__main__":
 
     # Parse arguments
-    parser = argparse.ArgumentParser(description="Render graph library dependencies")
-    parser.add_argument('-n', '--name', default='grapher', help='Name of the graph (Default is \'grapher\')')
+    parser = argparse.ArgumentParser(description="Render graph dependencies")
+    parser.add_argument('-n', '--name', default='set', help="Name of generated file(s) (default is \'set\')")
     parser.add_argument('-m', '--mode', default='file', choices=['file', 'module'], help="Granularity of the graph (Default is \'file\')")
-    parser.add_argument('-c', '--cleanup', default=False, action="store_true", help="Cleanup intermediate files")
-    parser.add_argument('-o', '--open', default=False, action="store_true", help="Open the rendered result with the default application")
+    parser.add_argument('-o', '--output', default='.', help='Directory output (default is execution directory)')
     parser.add_argument('-v', '--verbose', default=False, action="store_true", help="Display graph process in standard output")
-    parser.add_argument('--exist-only', default=False, action="store_true", help="Render only accessible files")
+    parser.add_argument('--known-only', default=False, action="store_true", help="Render only known files")
+    parser.add_argument('--cleanup', default=False, action="store_true", help="Cleanup intermediate files")
+    parser.add_argument('--view', default=False, action="store_true", help="Open the rendered result with the default application")
     parser.add_argument('dir', nargs='*', help="Directories containing files to graph")
 
     args = parser.parse_args()
     if args.verbose is True:
         print(args)
 
-    # Graph library
-    library = GraphLibrary(args.dir, args.name, args.mode, args.exist_only)
+    # Graph set
+    graph_set = GraphSet(args.dir, args.name, args.mode, args.output, args.known_only)
     if args.verbose is True and args.mode == 'file':
-        print(str(library))
+        print(str(graph_set))
     if args.verbose is True and args.mode == 'module':
-        print(repr(library))
+        print(repr(graph_set))
 
-    library.render(args.cleanup, args.open)
+    graph_set.render(args.cleanup, args.view)
